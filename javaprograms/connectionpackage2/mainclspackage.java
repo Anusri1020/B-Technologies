5110408a198a325ff2ee77a343c2a7ffa63a6f08
Anusri1020 <anubakki16@gmail.com>
2024-12-06 09:05:40 +0530

    Add files via upload

---
+++ javaprograms/connectionpackage2/mainclspackage.java
@@ -0,0 +1,16 @@
+package connectionpackage2;
+
+import connectionpackage1.a;
+import connectionpackage1.b;
+
+public class mainclspackage {
+	public static void main(String[]args) {
+		a obj=new a();
+		obj.hi();
+		b ibo=new b();
+		ibo.hello();
+	}
+
+}
+// it is connection to package of (package1 to pacakge2
+//it is besant connectity.)
